6e6a82b69b2dd6f959141c2364e195ae98b8ddfe
high on tantor <josephvenable@verizon.net>
2026-08-23 17:21:22 -0400

    Update window.cpp

--- source/windowing/glfw/window.cpp
+++ source/windowing/glfw/window.cpp
@@ -69,9 +69,7 @@ bool WindowGLFW::init(int w, int h, bool resizable, const std::string &title) {
 #elif defined(__APPLE__)
     widget_set_owner(std::to_string((unsigned long long)(void *)glfwGetCocoaWindow(window)).c_str());
 #elif (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
-	if (glfwGetPlatform() == GLFW_PLATFORM_X11) {
-		widget_set_owner(std::to_string((unsigned long long)(unsigned long)glfwGetX11Window(window)).c_str());
-	}
+	widget_set_owner(std::to_string((unsigned long long)(unsigned long)glfwGetX11Window(window)).c_str());
 #endif
 
     return true;
