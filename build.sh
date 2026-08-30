2e9d3828560a7f69059fbb95f49b428f0d58f139
high on tantor <josephvenable@verizon.net>
2026-08-29 22:56:04 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 cd "${0%/*}";
-if [ -d "build" ]; then
-	rm -fr build;
-fi;
-if [ ! -d "build" ]; then
-	mkdir build;
-fi;
-if [ -d "build" ]; then
-	cd build;
-	if [ "$OS" = "Windows_NT" ]; then
-		cmake .. -DCMAKE_BUILD_TYPE=Release -DSE_RENDERER="sdl3" -DSE_WINDOWING="sdl3" -DSE_AUDIO_ENGINE="sdl3" -DSE_CLOUDVARS=OFF -DSE_DOWNLOAD=OFF -DSE_USE_LIBRARY_BUILD=OFF && ninja;
-		cmake .. -DCMAKE_BUILD_TYPE=Release -DSE_RENDERER="sdl3" -DSE_WINDOWING="sdl3" -DSE_AUDIO_ENGINE="sdl3" -DSE_CLOUDVARS=OFF -DSE_DOWNLOAD=OFF -DSE_USE_LIBRARY_BUILD=ON && ninja;
-    else
-		cmake .. -DCMAKE_BUILD_TYPE=Release -DSE_RENDERER="sdl3" -DSE_WINDOWING="sdl3" -DSE_AUDIO_ENGINE="sdl3" -DSE_CLOUDVARS=OFF -DSE_DOWNLOAD=OFF -DSE_USE_LIBRARY_BUILD=OFF && make;
-		cmake .. -DCMAKE_BUILD_TYPE=Release -DSE_RENDERER="sdl3" -DSE_WINDOWING="sdl3" -DSE_AUDIO_ENGINE="sdl3" -DSE_CLOUDVARS=OFF -DSE_DOWNLOAD=OFF -DSE_USE_LIBRARY_BUILD=ON && make;
-	fi;
-fi;
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DSE_RENDERER="sdl3" -DSE_WINDOWING="sdl3" -DSE_AUDIO_ENGINE="sdl3" -DSE_CLOUDVARS=OFF -DSE_DOWNLOAD=OFF -DSE_USE_LIBRARY_BUILD=OFF && cmake --build build;
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DSE_RENDERER="sdl3" -DSE_WINDOWING="sdl3" -DSE_AUDIO_ENGINE="sdl3" -DSE_CLOUDVARS=OFF -DSE_DOWNLOAD=OFF -DSE_USE_LIBRARY_BUILD=ON && cmake --build build;
